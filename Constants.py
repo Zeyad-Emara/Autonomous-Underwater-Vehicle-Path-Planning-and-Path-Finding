f97a168063caaf81916107894b53356affb8987b
Zeyad-Emara <63409219+Zeyad-Emara@users.noreply.github.com>
2023-06-12 10:12:27 +0400

    Add files via upload

---
+++ Constants.py
@@ -0,0 +1,22 @@
+#Constants 
+GRID_WIDTH = 75  #Width of the grid, in cells
+GRID_HEIGHT = 75  #Height of the grid, in cells
+CELL_SIZE = 10  #Size of each cell in the grid, in pixels
+NUM_GOALS = 5  #Number of goals to generate
+BG_COLOR = (20, 100, 200)  #Background color of the grid
+AUV_COLOR = (255 ,255, 0)  #Color of the AUV agent
+GOAL_COLOR = (255, 0, 0)  #Color of the goals
+DONE_GOAL_COLOR = (128, 0, 128)  # Color of reached goals
+BORDER_COLOR = (0, 0, 0)  #Color of the cell borders
+OBSTACLE_COLOR = (139, 69, 19) #Color of the obstacles
+OBSTACLE_DENSITY_LOW = 0.1 #Low obstacel density
+OBSTACLE_DENSITY_MEDIUM = 0.25 #Medium obstacel density
+OBSTACLE_DENSITY_HIGH = 0.4 #High obstacel density
+SMALL = (50, 50) #Small enviroment size
+MEDIUM = (75, 75) #Medium enviroment size
+LARGE = (150, 150) #Large enviroment size
+BUTTON_COLOR = (100, 100, 255) 
+BUTTON_HOVER_COLOR = (150, 150, 255) 
+TEXT_COLOR = (255, 255, 255) #Button text color
+FONT_SIZE = 20 #Button font size
+DETECTION_RADIUS = 5 #Detection Range Radius
